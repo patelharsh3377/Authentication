be263726b05134905b8a15bcd2e6f5934bbc6c46
Harsh3377 <patelharsh15062002@gmail.com>
2023-09-22 15:39:10 +0530

    commit on 21/09/2023

--- style.js
+++ style.js
@@ -36,24 +36,44 @@ function checkregistation() {
 
     if (Username === "") {
         console.log("enter a valid Username")
+        document.getElementById('usernameError').textContent ="enter a valid Username"
+        document.getElementById('usernameError').style.color = "red"
+        
     }
     else if (Youremail === "") {
         console.log("Enter a valid Email id")
+        document.getElementById('emailerror').textContent ="enter a valid email id"
+        document.getElementById('emailerror').style.color = "red"
     }
     else if (Password === "") {
         console.log("Enter a valid password")
+        document.getElementById('Passworderror').textContent ="Enter a valid password"
+        document.getElementById('Passworderror').style.color = "red"
+
     }
     else if (DOB === "") {
         console.log("Enter a valid Dob")
+        document.getElementById('DOBerror').textContent ="Enter a valid Dob"
+        document.getElementById('DOBerror').style.color = "red"
+
     }
     else if (City === "none") {
         console.log("Enter a valid City")
+        document.getElementById('Cityerror').textContent ="Enter a valid city"
+        document.getElementById('Cityerror').style.color = "red"
+
     }
     else if (selectedGender === "") {
         console.log(" selected Gender")
+        document.getElementById('Gendererror').textContent ="selected Gender"
+        document.getElementById('Gendererror').style.color = "red"
+
     }
     else if (Password.length < 3) {
         console.log("Enter 3 letter password")
+        document.getElementById('Passworderror').value ="Enter 3 letter password"
+
+
     }
     else {
         console.log(Username);
